8a7718e7c9af72f86a95c921c4505ebdbd34790e
Giang Vu <vuminhgiangmr.bean@gmail.com>
2018-01-29 11:31:39 +0700

    Organized files

--- assets/js/topLap.js
+++ assets/js/topLap.js
@@ -1,8 +1,10 @@
 /* Add a CSS class to all fa-icons on navigation bar to make them visible when view on mobile devices
 (and responsive mode) */
-var faIcon = document.querySelectorAll("li a i.fa"); // select
+var faIcons = document.querySelectorAll("li a i.fa"); // select
 // manipulate
 // loop through the list of all fa-icons on navbar, then give them the class "navbar__icon--mobile"
 for (var i = 0; i < faIcon.length; i++) {
-    faIcon[i].classList.add("navbar__icon--mobile");
+    faIcons[i].classList.add("navbar__icon--mobile");
 }
+//
+
